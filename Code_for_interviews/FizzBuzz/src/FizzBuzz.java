bb3b975ee750b1127dc7397c217540651361f874
AptFox <AptFox@users.noreply.github.com>
2018-11-29 20:48:38 -0500

    updated FizzBuzz

--- Code_for_interviews/FizzBuzz/src/FizzBuzz.java
+++ Code_for_interviews/FizzBuzz/src/FizzBuzz.java
@@ -1,12 +1,9 @@
-/*Write a short program that prints each number from 1 to 100 on a new line. 
-
-For each multiple of 3, print "Fizz" instead of the number. 
-
-For each multiple of 5, print "Buzz" instead of the number. 
-
-For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.*/
+/*	Write a short program that prints each number from 1 to 100 on a new line. 
+	For each multiple of 3, print "Fizz" instead of the number. 
+	For each multiple of 5, print "Buzz" instead of the number. 
+	For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+*/
 public class FizzBuzz {
-
 	public static void main(String[] args) {
 		for(int i=1; i<=100; i++) {
 			/*Solution uses the conditional or ternary operator to make decisions instead of if statements.
@@ -20,7 +17,5 @@ public class FizzBuzz {
 			   else return int value of "i" with an empty string appended to cast to String */
 			System.out.println(i%3==0 && i%5==0 ? "FizzBuzz" : i%3==0 ? "Fizz": i%5==0 ? "Buzz" : i+"");
 		}
-
 	}
-
 }
