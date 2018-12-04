7823fff04ba2c533018574f5e6ade73baf0086a6
AptFox <AptFox@users.noreply.github.com>
2018-12-03 22:27:10 -0500

    Updated FizzBuzz example to include a regular solution in addition to the one liner

--- Code_for_interviews/FizzBuzz/src/FizzBuzz.java
+++ Code_for_interviews/FizzBuzz/src/FizzBuzz.java
@@ -4,18 +4,21 @@
 	For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
 */
 public class FizzBuzz {
+
 	public static void main(String[] args) {
 		for(int i=1; i<=100; i++) {
-			/*Solution uses the conditional or ternary operator to make decisions instead of if statements.
-			  It's essentially one big assignment inside of a print statement. Whatever the return become is
-			  what's printed to the console. 
-			  
-			   Logically it reads like this:
-			   if "i" is divisible by 3 and 5 return "FizzBuzz"
-			   else if "i" is divisible by 3 return "Fizz"
-			   else if "i" is divisible by 5 return "Buzz"
-			   else return int value of "i" with an empty string appended to cast to String */
-			System.out.println(i%3==0 && i%5==0 ? "FizzBuzz" : i%3==0 ? "Fizz": i%5==0 ? "Buzz" : i+"");
+			String num = i+"";
+			if (i%3 == 0 && i%5 == 0) {
+				num = "FizzBuzz";
+			}
+			else if (i%3== 0) {
+				num = "Fizz";
+			}
+			else if (i%5 == 0) {
+				num = "Buzz";
+			}
+			System.out.println(num);
 		}
 	}
+
 }
---
+++ Code_for_interviews/FizzBuzz/src/FizzBuzzInOneLine.java
@@ -0,0 +1,21 @@
+/*	Write a short program that prints each number from 1 to 100 on a new line. 
+	For each multiple of 3, print "Fizz" instead of the number. 
+	For each multiple of 5, print "Buzz" instead of the number. 
+	For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+*/
+public class FizzBuzzInOneLine {
+	public static void main(String[] args) {
+		for(int i=1; i<=100; i++) {
+			/*Solution uses the conditional or ternary operator to make decisions instead of if statements.
+			  It's essentially one big assignment inside of a print statement. Whatever the return become is
+			  what's printed to the console. 
+			  
+			   Logically it reads like this:
+			   if "i" is divisible by 3 and 5 return "FizzBuzz"
+			   else if "i" is divisible by 3 return "Fizz"
+			   else if "i" is divisible by 5 return "Buzz"
+			   else return int value of "i" with an empty string appended to cast to String */
+			System.out.println(i%3==0 && i%5==0 ? "FizzBuzz" : i%3==0 ? "Fizz": i%5==0 ? "Buzz" : i+"");
+		}
+	}
+}
