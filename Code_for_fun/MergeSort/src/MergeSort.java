3ba3b0cfa3282c6c117d1d4714ce167bd7b279c4
AptFox <AptFox@users.noreply.github.com>
2019-01-09 23:53:55 -0500

    updated mergeSort a bit

--- Code_for_fun/MergeSort/src/MergeSort.java
+++ Code_for_fun/MergeSort/src/MergeSort.java
@@ -13,57 +13,49 @@ public class MergeSort {
 	
 	public static List<Integer> mergeSort(List<Integer> intList) {
 		int arraySize = intList.size();
-		//List<Integer> mergeList = new ArrayList<Integer>();
-		List<Integer> leftList = new ArrayList<Integer>();
-		List<Integer> rightList = new ArrayList<Integer>();
-		
-		leftList.addAll(intList);
-		rightList.addAll(intList);
-		
-		if(arraySize > 1) {
-			int indexOfMiddleValue = arraySize/2;
-			leftList = mergeSort(leftList.subList(0, indexOfMiddleValue));
-			rightList = mergeSort(rightList.subList(indexOfMiddleValue, arraySize-1));
-		}else{
+		if(arraySize ==1) {
 			return intList;
 		}
-		
-		//mergeList.addAll(leftList);
-		if(leftList.isEmpty() || rightList.isEmpty()) {
-			leftList.addAll(rightList);
-			return leftList;
-		}else {
-			for(int value : rightList) {
-				int indexForValue = findIndexInList(value, leftList);
-				leftList.add(indexForValue, value);
-			}
-		}
-			
-		return leftList;
+		int indexOfMiddleValue = arraySize/2;
+		List<Integer> leftList = createSubList(intList, indexOfMiddleValue-1, false);
+		List<Integer> rightList = createSubList(intList, indexOfMiddleValue, true);
+		return merge(mergeSort(leftList), mergeSort(rightList));
+
 	}
 	
-	//Take an integer and a list of integers
-	//Returns the index where the integer should be placed in the list of integers. 
-	public static int findIndexInList(int value, List<Integer> intList) {
-		int index = 0;
-		for(int listValue : intList) {
-			if(value > listValue) {
-				index = intList.indexOf(listValue);
-				index++;
-			}else if(value < listValue) {
-				index = intList.indexOf(listValue);
-				index--;
-			}else if (value == listValue){
-				index = intList.indexOf(listValue);
+	public static List<Integer> createSubList(List<Integer> oldList, Integer splitIndex, boolean countingUp){
+		List<Integer> newList = new ArrayList<Integer>();
+		if(countingUp) {
+			for(int i = splitIndex; i < oldList.size(); i++ ) {
+				newList.add(oldList.get(i));
+			}
+		}else {
+			for(int i = splitIndex; i > -1; i--) {
+				newList.add(oldList.get(i)); 
 			}
 		}
-		if(index < 0) {
-			index = 0;
-		}
-		if(index > intList.size() -1) {
-			index = intList.size() -1;
-		}
-		return index;
+		return newList;
 	}
+	
+	public static List<Integer> merge (List<Integer> leftList, List<Integer> rightList){
+		int leftIndex = 0;
+		int rightIndex = 0;
+		List<Integer> mergedList = new ArrayList<Integer>(); 
+		while(leftIndex < leftList.size() && rightIndex < rightList.size()) {
+			//The problem lives here one the elements always gets dropped because 
+			//one side of the while statement becomes false after adding an element to the mergedList
+			//I need a way of adding both elements. 
+			//due to the dropping this method currently keeps dropping values until only the largest remains. 
+			if(leftList.get(leftIndex) > rightList.get(rightIndex)) {
+				mergedList.add(leftList.get(leftIndex));
+				leftIndex++;
+			}else {
+				mergedList.add(rightList.get(rightIndex));
+				rightIndex++;
+			}
+		}
+		return mergedList;
+	}
+	
 
 }
