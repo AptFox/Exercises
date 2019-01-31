0a779677a30afaafcd2f9fb7c1b379607f9653d1
AptFox <AptFox@users.noreply.github.com>
2019-01-31 01:00:19 -0500

    completed bowling calculator.

--- Code_for_interviews/Bowling/src/BowlingScoreCalculator.java
+++ Code_for_interviews/Bowling/src/BowlingScoreCalculator.java
@@ -1,9 +1,5 @@
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-
 /*
  * Prompt: 
  * 	Create a program, which, given a valid sequence of rolls for one line of American Ten-Pin Bowling, 
@@ -26,74 +22,45 @@ public class BowlingScoreCalculator {
     };
 	
 	public int ScoreGame(String line) {
-		return ScoreGame(line.replaceAll("\\s", ""), 1);
+		return ScoreGame(line.replaceAll("\\s", ""), null);
 	}
 	
 	public int ScoreGame(String line, Integer frame) {
 		frame = frame == null ? 1 : frame;
 		boolean onLastFrame = frame == 10;
-		if(line.length() == 0) {
-			return 0;
-		}else if (line.length() == 1) {
-			return convertCharToScore(line.charAt(0));
-		}
+		boolean takeSubStringAtIndexOne = false;
+		int scoreForCurrentFrame;
 		Character throwOne = line.charAt(0);
 		Character throwTwo = line.charAt(1);
-		Character throwThree;
-		if(throwOne == 'X') {
+		Character throwThree = null;
+		if (throwOne == 'X' || throwTwo == '/') {
 			throwThree = line.charAt(2);
-			if(onLastFrame) {
-				return getScoreOfNextThrows(throwOne, throwTwo, throwThree);
-			}else {
-				return getScoreOfNextThrows(throwOne, throwTwo, throwThree)+ScoreGame(line.substring(1), frame++);
-			}
-		}
-		else if(throwTwo == '/') {
-			throwThree = line.charAt(2);
-			if(onLastFrame) {
-				return getScoreOfNextThrows(throwTwo, throwThree);
-			}else {
-				return getScoreOfNextThrows(throwTwo,throwThree)+ScoreGame(line.substring(2), frame++);
-			}
-		}
-		else if (throwOne == '-') {
-			return convertCharToScore(throwTwo)+ScoreGame(line.substring(1), frame++);
-		}
-		else if (throwTwo == '-') {
-			return convertCharToScore(throwOne)+ScoreGame(line.substring(2), frame++);
+			if(throwOne == 'X') { takeSubStringAtIndexOne = true; }
+			else {throwOne = null;}
 		}
 		else {
-			return convertCharToScore(throwOne) + convertCharToScore(throwTwo) + ScoreGame(line.substring(2), frame++);
+			if (throwOne == '-') {
+				throwOne = null;
+				takeSubStringAtIndexOne = true;
+			}
+			else if (throwTwo == '-') { throwTwo = null; }
 		}
-		
-		 
+		scoreForCurrentFrame = getScoreOfNextThrows(throwOne, throwTwo, throwThree);
+		if(onLastFrame) {return scoreForCurrentFrame;}
+		else {
+			frame+=1;
+			int subStringIndex = takeSubStringAtIndexOne ? 1 : 2;
+			return scoreForCurrentFrame+ScoreGame(line.substring(subStringIndex), frame);
+		}
 	}
-	
-	private int convertCharToScore(Character ch) {
-		int score;
-		if(Character.isDigit(ch)) {
-			score = Character.getNumericValue(ch);
-		}else {
-			score = scoreMap.get(ch);
+
+	private int getScoreOfNextThrows(Character...chars) {
+		int score = 0;
+		for(Character ch : chars) {
+			if(ch == null) { score+=0; }
+			else if(Character.isDigit(ch)) {score += Character.getNumericValue(ch);}
+			else { score += scoreMap.get(ch);}
 		}
 		return score;
 	}
-
-
-	private int getScoreOfNextThrows(Character...bowls) {
-		int frameScore = 0;
-		if(bowls.length == 1) {
-			return convertCharToScore(bowls[0]);
-		}else {
-			for(Character ch : bowls) {
-				frameScore += convertCharToScore(ch);
-			}
-		}
-		return frameScore;
-	}
-	
-
-
-
-
 }
--- Code_for_interviews/Bowling/src/BowlingScoreCalculatorTest.java
+++ Code_for_interviews/Bowling/src/BowlingScoreCalculatorTest.java
@@ -12,17 +12,33 @@ class BowlingScoreCalculatorTest {
 		int actualScore = bsc.ScoreGame(frameScores);
 		assertEquals(expectedScore, actualScore);
 	}
-	
+	@Test
 	void testCalculateScore_AllNines() {
 		String frameScores ="9- 9- 9- 9- 9- 9- 9- 9- 9- 9-";
 		int expectedScore = 90;
 		int actualScore = bsc.ScoreGame(frameScores);
 		assertEquals(expectedScore, actualScore);
 	}
-	
+	@Test
 	void testCalculateScore_FivesAndSpares() {
 		String frameScores = "5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5";
 		int expectedScore = 150;
+		int actualScore = bsc.ScoreGame(frameScores);
+		assertEquals(expectedScore, actualScore);
+	}
+	
+	@Test
+	void testCalculateScore_EightsAndOnes() {
+		String frameScores = "81 81 81 81 81 81 81 81 81 81";
+		int expectedScore = 90;
+		int actualScore = bsc.ScoreGame(frameScores);
+		assertEquals(expectedScore, actualScore);
+	}
+	
+	@Test
+	void testCalculateScore_RandomThrows() {
+		String frameScores = "12 34 52 71 9/ 1- 11 12 13 14";
+		int expectedScore = 51;
 		int actualScore = bsc.ScoreGame(frameScores);
 		assertEquals(expectedScore, actualScore);
 	}
