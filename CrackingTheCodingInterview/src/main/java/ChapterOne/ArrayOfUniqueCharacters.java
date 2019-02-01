19557c42167d528cb5fef5dbac76c3d40effc835
AptFox <AptFox@users.noreply.github.com>
2019-02-01 17:08:20 -0500

    Added crackingTheCodingInterview Exercises

---
+++ CrackingTheCodingInterview/src/main/java/ChapterOne/ArrayOfUniqueCharacters.java
@@ -0,0 +1,24 @@
+/*
+ * Prompt: Implement an algorithm to determine if a string has all unique characters
+ */
+package main.java.ChapterOne;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayOfUniqueCharacters {
+	public static boolean checkForUnique(String tString) {
+		boolean stringIsUnique = true;
+		char[] chars = tString.toCharArray();
+		List<Character> charsAlreadySeen = new ArrayList<Character>();
+		for(char ch : chars) {
+			if(charsAlreadySeen.contains(ch)) {
+				stringIsUnique = false;
+				break;
+			}else {
+				charsAlreadySeen.add(ch);
+			}
+		}
+		return stringIsUnique;
+	}
+}
---
+++ CrackingTheCodingInterview/src/test/java/ChapterOne/ArrayOfUniqueCharactersTest.java
@@ -0,0 +1,51 @@
+package test.java.ChapterOne;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import main.java.ChapterOne.ArrayOfUniqueCharacters;
+
+class ArrayOfUniqueCharactersTest {
+
+	@Test
+	void testCheckForUniqueWith123() {
+		String tString = "123";
+		boolean expected = true;
+		boolean actual = ArrayOfUniqueCharacters.checkForUnique(tString);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testCheckForUniqueWithABC() {
+		String tString = "ABC";
+		boolean expected = true;
+		boolean actual = ArrayOfUniqueCharacters.checkForUnique(tString);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testCheckForUniqueWithOneOneOne() {
+		String tString = "111";
+		boolean expected = false;
+		boolean actual = ArrayOfUniqueCharacters.checkForUnique(tString);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testCheckForUniqueWithLotsOfUniqueChars() {
+		String tString = "123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*() {}[]-=<>,.?/`~";
+		boolean expected = true;
+		boolean actual = ArrayOfUniqueCharacters.checkForUnique(tString);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testCheckForUniqueWithLotsOfNonUniqueChars() {
+		String tString = "1234567891011121314151617181920ABCabc123321";
+		boolean expected = false;
+		boolean actual = ArrayOfUniqueCharacters.checkForUnique(tString);
+		assertEquals(expected, actual);
+	}
+
+}
