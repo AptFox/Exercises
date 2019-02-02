be0eed48c8f6aea0abf7f24e8791124fc088f630
AptFox <AptFox@users.noreply.github.com>
2019-02-01 19:05:09 -0500

    C1Q2: Solution updated

--- CrackingTheCodingInterview/src/main/java/ChapterOne/Q2/CheckTwoStringsForPermutation.java
+++ CrackingTheCodingInterview/src/main/java/ChapterOne/Q2/CheckTwoStringsForPermutation.java
@@ -1,19 +1,17 @@
 package main.java.ChapterOne.Q2;
 
+import java.util.Arrays;
+
 public class CheckTwoStringsForPermutation {
 
 	public static boolean checkForPermutations(String strOne, String strTwo) {
-		boolean isPermutation = true;
 		if (strOne.length() != strTwo.length()) {
 			return false;
 		}
-		char[] chars = strOne.toCharArray();
-		for(char ch : chars) {
-			if(!strTwo.contains(ch+"")) {
-				isPermutation = false;
-				break;
-			}
-		}
-		return isPermutation;
+		char[] charsOne = strOne.toCharArray();
+		char[] charsTwo = strTwo.toCharArray();
+		Arrays.sort(charsOne);
+		Arrays.sort(charsTwo);
+		return Arrays.equals(charsOne, charsTwo);
 	}
 }
