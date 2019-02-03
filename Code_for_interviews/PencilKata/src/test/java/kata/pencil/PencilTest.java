a7ed15bfa42ca19c63a6d1912c4beecfc6e087fe
AptFox <AptFox@users.noreply.github.com>
2019-02-02 22:01:45 -0500

    Initial commit for pencil durability kata

---
+++ Code_for_interviews/PencilKata/src/test/java/kata/pencil/PencilTest.java
@@ -0,0 +1,14 @@
+package test.java.kata.pencil;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PencilTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
