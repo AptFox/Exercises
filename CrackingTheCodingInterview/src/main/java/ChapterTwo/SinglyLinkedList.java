32f9dc0938244424681af13ca65e1d07ffbd3d88
AptFox <AptFox@users.noreply.github.com>
2019-02-16 15:13:20 -0500

    Updated SinglyLinkedList

--- CrackingTheCodingInterview/src/main/java/ChapterTwo/SinglyLinkedList.java
+++ CrackingTheCodingInterview/src/main/java/ChapterTwo/SinglyLinkedList.java
@@ -13,9 +13,10 @@ public class SinglyLinkedList {
 	public void deleteNode(Node node) {
 		Node nodeToDelete = headNode;
 		Node previousNode = null;
+
 		//iterate through nodes until you find one that matches
 		// the supplied node
-		while(nodeToDelete.data != node.data) {
+		while(nodeToDelete != null && nodeToDelete.data != node.data) {
 			//keep track of the node that came before
 			previousNode = nodeToDelete;
 			//Move to the next node
@@ -23,7 +24,7 @@ public class SinglyLinkedList {
 		}
 		//Once found, set the previous nodes 'next' reference
 		//to the Node that comes after the node we plan to delete
-		//The node is effectively delete by being dropped from the LinkedList. 
+		//The node is effectively deleted by being dropped from the LinkedList. 
 		previousNode.next = nodeToDelete.next;
 	}
 	
