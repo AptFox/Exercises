16423da1343d9ec65964d3f75eac9322d3fb637e
AptFox <AptFox@users.noreply.github.com>
2019-05-09 16:02:04 -0400

    implemented basic add and subtract

--- PillarKatas/RomanToArabic_Vanilla/RomanToArabicConverter.js
+++ PillarKatas/RomanToArabic_Vanilla/RomanToArabicConverter.js
@@ -1,14 +1,54 @@
 let converter = {};
+var numeralMap = new Map([[undefined, ""],["I", 1],["V",5],["X",10]]);
 
 converter.convertToArabic = function(numeral) { //function name
-    var convertedString;
-    if (numeral === undefined) {
+    var convertedString = numeral;
+    var numeralIsDefined = typeof numeral !== 'undefined';
+
+    if(!numeralIsDefined){
         convertedString = "";
     }
-    if(numeral === "I"){
-        convertedString = "1";
+    else if(numeralIsDefined && numeral.length === 1){
+        convertedString = numeralMap.get(numeral);
+    }else{
+        //split string into an array
+        var numeralArray = numeral.split("");
+        //convert each pos of array to arabic
+        var largest = 0;
+        for(let i = 0; i < numeralArray.length; i++){
+            numeralArray[i] = numeralMap.get(numeralArray[i]);
+            //find largest pos
+            if(numeralArray[i] > largest){
+                largest =  numeralArray[i];
+            }
+        }
+        var largestPos = numeralArray.indexOf(largest);
+        if(largestPos > 0){
+            var subtractVal = numeralArray[largestPos-1];
+            convertedString = largest - subtractVal;
+/*            for(let i = largestPos-1; i > 0; i--){
+                //each iteration works in two's
+                //get positive val for right pos1
+                //subtract left val from pos1
+                subtractVal -= numeralArray[i];
+            }*/
+        }
+        else if (largestPos < numeralArray.length){
+            var addVal = numeralArray[largestPos];
+            for(let i = largestPos+1; i < numeralArray.length; i++){
+                addVal += numeralArray[i];
+            }
+            convertedString  = addVal;
+        }
     }
-    return convertedString;
+    return convertedString.toString();
 };
+
+//store each char from string in an array
+    //iterate through array and convert each position to arabic equivalent
+    //find the largest element in the array
+    //add up all element to the left and subtract from largest
+    //add up all elements to the right and add to largest
+//return (this might work with recursion)
 
 module.exports = converter;
--- PillarKatas/RomanToArabic_Vanilla/test/testRomanToArabicConverter.js
+++ PillarKatas/RomanToArabic_Vanilla/test/testRomanToArabicConverter.js
@@ -2,12 +2,34 @@ let converter = require('../RomanToArabicConverter.js');
 let assert = require('assert');
 
 describe('RomanToArabicConverter', function() {
-    describe('convertToArabic', function() {
+    describe('convertSingleNumeralsToArabic', function() {
         it('should return zero when called with empty args', function() {
             assert.strictEqual(converter.convertToArabic(), "");
         });
         it('should return 1 when called with "I"', function() {
             assert.strictEqual(converter.convertToArabic("I"), "1");
+        });
+        it('should return 5 when called with "V"', function() {
+            assert.strictEqual(converter.convertToArabic("V"), "5");
+        });
+        it('should return 10 when called with "X"', function() {
+            assert.strictEqual(converter.convertToArabic("X"), "10");
+        });
+    });
+
+    describe('convertMultipleNumeralsToArabic', function() {
+        describe('subtraction', function() {
+            it('should return 4 when called with "IV"', function() {
+                assert.strictEqual(converter.convertToArabic("IV"), "4");
+            });
+        });
+        describe('addition', function() {
+            it('should return 6 when called with "VI"', function() {
+                assert.strictEqual(converter.convertToArabic("VI"), "6");
+            });
+            it('should return 8 when called with "VIII"', function() {
+                assert.strictEqual(converter.convertToArabic("VIII"), "8");
+            });
         });
     });
 });
